fa4099b7b55e50fd38b2ee553fbd9d9d622e91f9
“laststone” <yang.hu@cloudwiz.cn>
2019-10-17 08:58:25 +0000

    新增log-analysis模板的特殊替换

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -451,5 +451,6 @@ done
 
 sed -i 's/daemon off;/#daemon off;/' $install_root/nginx/conf/nginx.conf
 sed -i '/^CMService.HttpHost =/cCMService.HttpHost = 0.0.0.0' $install_root/cmservice/conf/cmservice.properties
+sed -i '/^LogClustering.HttpHost =/cLogClustering.HttpHost = 0.0.0.0' $install_root/log-analysis/config/log.analysis.properties
 
 exit 0
