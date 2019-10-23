b65460b8b1838f8b4756e652e3dd9ec9ff8f1b3b
“laststone” <yang.hu@cloudwiz.cn>
2019-10-23 10:36:13 +0000

    新增等待hbase-master脚本

--- alertd/start.sh
+++ alertd/start.sh
@@ -1,18 +1,52 @@
 #!/bin/bash
 
-while true;do
-    _status=$(curl -s "http://hbase-master:16010/master-status?format=json")
+# make sure HMaster is ready
+echo "Waiting for HMaster to get ready"
+NAME=hbase-master
+PORT=16010
+_hmaster_hosts=( $NAMaE )
+_ready=0
+while [ 1 ]
+do
+    _done=0
+    _hmaster_url="http://hbase-master:16010/master-status?format=json"
+    _status=$(curl -s "$_hmaster_url")
+
     if [ "_${_status//[[:space:]]/}_" = "_[]_" ]; then
-        echo "hbase-master is ready ..."
+        _done=1
+    fi
+    echo "status: $_status"
+    echo "done: $_done"
+    echo "_ready: $_ready"
+
+    if [ $_done -ne 0 ]; then
+        _ready=$((_ready+1))
+        echo "HMaster is getting ready"
+        sleep 2
+    else
+        _ready=0
+        echo "HMaster is not ready"
+        sleep 10
+    fi
+
+    if [ $_ready -gt 8 ]; then
+        echo "HMaster is ready"
+        sleep 10
+        break
+    fi
+done
+
+while true;do
+    _status=$(curl -I -m 10 -o /dev/null -s -w %{http_code}  http://hbase-master:16010/table.jsp?name=vsms_callbackInfo)
+    if [[ $_status -eq 200 ]];then
+        echo "hbase-table is ready ..."
+        echo "staring alertd ..."
+        sleep 10
         break
     else
-        echo "wating for hbase-master ..."
+        echo "waiting for hbase-tables ..."
         sleep 2
     fi
 done
 
-for i in {1..50};do
-    echo "starting alertd ... $i"
-    sleep 1
-done
 /opt/alertd/bin/start.sh
