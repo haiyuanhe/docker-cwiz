2f8d78763ade73499035b623523b41b47c80afde
laststone <yang.hu@cloudwiz.cn>
2019-12-08 10:08:16 +0000

    在mysql镜像中设置默认密码

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -109,9 +109,10 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 	_check_config "$@"
 	# Get config
 	DATADIR="$(_get_config 'datadir' "$@")"
+        MYSQL_ROOT_PASSWORD=$(echo -n "Q2xvdWR3aXpfcDBj" | base64 -d)
 
 	if [ ! -d "$DATADIR/mysql" ]; then
-		file_env 'MYSQL_ROOT_PASSWORD'
+		#file_env 'MYSQL_ROOT_PASSWORD'
 		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" -a -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
 			echo >&2 'error: database is uninitialized and password option is not specified '
 			echo >&2 '  You need to specify one of MYSQL_ROOT_PASSWORD, MYSQL_ALLOW_EMPTY_PASSWORD and MYSQL_RANDOM_ROOT_PASSWORD'
