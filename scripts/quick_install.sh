706439e14a8cf2f841f8e48482bd49e798834e6e
hehaiyuan <hehaiyuan.hhy@gmail.com>
2019-12-23 18:20:51 +0800

    加入快捷安装方式. 修改日志. 修改mysql binlog 绑定地址

--- scripts/quick_install.sh
+++ scripts/quick_install.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
-
 read -p "Please input the local ip: " local_ip
-echo -e "\n"
+read -r -p "Is the local IP the same as the external IP (EIP) ? [y/N] " input
+case $input in
+    [yY][eE][sS]|[yY])
+		eip=$local_ip
+		;;
+
+    [nN][oO]|[nN])
+       	read -p "Please input the external IP (EIP): " eip
+       	;;
+
+    *)
+	echo "Invalid input..."
+	exit 1
+	;;
+esac
 read -p "Please input the CloudOps url: " huawei_url
-echo -e "\n"
-
-sed -i '/^huawei_url =/chuawei_url = ${huawei_url}' ./install.ini
-sed -i '/^nginx_ext_ip =/cnginx_ext_ip = ${$local_ip}' ./install.ini
-sed -i '/^docker_host_int_ip =/cdocker_host_int_ip = ${$local_ip}' ./install.ini
+read -p "Please input the CloudOps access key [468f917903df145ee2e9]: " ak
+read -p "Please input the CloudOps secret key [1297d4cbc503fbf25d36335b2e4e3993]: " sk
 
 
+echo "local ip:             ${local_ip}"
+echo "external IP (EIP):    ${eip}"
+echo "CloudOps URL:         ${huawei_url}"
+echo "CloudOps access key:  ${ak}"
+echo "CloudOps secret key:  ${sk}"
+
+sed -i "/^huawei_url =/chuawei_url = ${huawei_url}" ./install.ini
+sed -i "/^huawei_ak =/chuawei_ak = ${ak}" ./install.ini
+sed -i "/^huawei_sk =/chuawei_sk = ${sk}" ./install.ini
+sed -i "/^nginx_ext_ip =/cnginx_ext_ip = ${eip}" ./install.ini
+sed -i "/^docker_host_int_ip =/cdocker_host_int_ip = ${local_ip}" ./install.ini
+
+read -n 1 -p "Press any key to continue..."
 ./install-all.sh ./install.ini
