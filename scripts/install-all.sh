d8e10b64b9d943e6b12b083a6973626e7b240682
huangbiao <h295203236@163.com>
2019-12-24 21:40:57 +0800

    add krb5.conf when start install.

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -472,5 +472,5 @@ bash $install_root/agent/bin/repackage.sh
 chown -R 101:101 $install_root/nginx/
 bash $install_root/tools/certs/create-ssl-all-x.sh
 bash $install_root/tools/certs/create-mysql-ssl.sh
-touch $install_root/etc/krb5.conf
+mdkir -p $install_root/etc && touch $install_root/etc/krb5.conf
 exit 0
