e86eb9d4d9a95eb3a78b81f57e8f89b78b65c48f
hehaiyuan <hehaiyuan.hhy@gmail.com>
2019-12-28 11:37:47 +0800

    加入 vip

--- scripts/quick_install.sh
+++ scripts/quick_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ak=468f917903df145ee2e9
+sk=1297d4cbc503fbf25d36335b2e4e3993
 read -p "Please input the local ip: " local_ip
 read -r -p "Is the local IP the same as the external IP (EIP) ? [y/N] " input
 case $input in
@@ -15,18 +17,21 @@ case $input in
 	exit 1
 	;;
 esac
-read -p "Please input the CloudOps url: " huawei_url
+read -p "Please input the CloudOps local url: " huawei_url
+read -p "Please input the CloudOps virtual url: " huawei_vurl
 read -p "Please input the CloudOps access key [468f917903df145ee2e9]: " ak
 read -p "Please input the CloudOps secret key [1297d4cbc503fbf25d36335b2e4e3993]: " sk
 
 
 echo "local ip:             ${local_ip}"
 echo "external IP (EIP):    ${eip}"
-echo "CloudOps URL:         ${huawei_url}"
+echo "CloudOps local URL:   ${huawei_url}"
+echo "CloudOps virtual URL: ${huawei_vurl}"
 echo "CloudOps access key:  ${ak}"
 echo "CloudOps secret key:  ${sk}"
 
 sed -i "/^huawei_url =/chuawei_url = ${huawei_url}" ./install.ini
+sed -i "/^huawei_vurl =/chuawei_vurl = ${huawei_vurl}" ./install.ini
 sed -i "/^huawei_ak =/chuawei_ak = ${ak}" ./install.ini
 sed -i "/^huawei_sk =/chuawei_sk = ${sk}" ./install.ini
 sed -i "/^nginx_ext_ip =/cnginx_ext_ip = ${eip}" ./install.ini
