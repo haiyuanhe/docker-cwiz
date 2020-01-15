96702e5cee737a40283ca04a0ec37ae5c224c2d5
Keqing <407066271@qq.com>
2020-01-15 11:16:25 +0800

    修改配置

--- datanode/run.sh
+++ datanode/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-datadir="/data/dfs/datanode"
+datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
 if [ ! -d $datadir ]; then
   echo "Datanode data directory not found: $datadir"
   exit 2
